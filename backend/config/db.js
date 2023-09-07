9f85d1363669ab895794836e5c9364e1140f6be7
KudakwasheM <masayakudakwashe@gmail.com>
2023-09-07 10:33:26 +0200

    Mongo uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://netoprojects:Neto1012@netocluster.yifesqp.mongodb.net/middle?retryWrites=true&w=majority"
+    );
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.magenta);
   } catch (error) {
     console.log(`Error: ${error.message}`.red);
