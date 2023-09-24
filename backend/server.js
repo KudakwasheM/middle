89b200a2c198df2f8c4aa6aa2bbdc2deef02cc6a
KudakwasheM <masayakudakwashe@gmail.com>
2023-09-24 14:14:59 +0200

    Cross origin test fix

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(cookieParser());
 
